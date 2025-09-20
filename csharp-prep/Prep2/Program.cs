883313268f36f899c93fa2874355d4611075b535
Sugarbaby4 <lew23028@byui.edu>
2025-09-20 16:37:19 -0600

    Grade letter project

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,75 @@
 using System;
+using System.Security.Cryptography;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("What is your grade precentage? ");
+        string answer = Console.ReadLine();
+        int number = int.Parse(answer);
+
+        string letter = "";
+
+        if (number >= 90)
+        {
+            letter = "A";
+        }
+        else if (number >= 80)
+        {
+            letter = "B";
+        }
+        else if (number >= 70)
+        {
+            letter = "C";
+        }
+        else if (number >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+// Stretch challenge //
+
+        string sign = "";
+
+        int lastdigit = number % 10;
+
+        if (lastdigit >= 7)
+        {
+            sign = "+";
+        }
+        else if (lastdigit < 3)
+        {
+            sign = "-";
+        }
+        else
+        {
+            sign = "";
+        }
+
+        if (number >= 93)
+        {
+            sign = "";
+        }
+
+        if (letter == "F")
+        {
+            sign = "";
+        }
+
+        Console.WriteLine($"Your grade is: {letter}{sign}");
+
+        if (number >= 70)
+        {
+            Console.WriteLine("Congragulations you passed!!!");
+        }
+        else
+        {
+            Console.WriteLine("Sorry you failed. Best of luck next time!");
+        }
     }
 }
