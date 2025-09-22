16830a5276e0c24eb9b10ddb4a5d09996c3fe0b5
Sugarbaby4 <lew23028@byui.edu>
2025-09-22 09:42:21 -0600

    New sandbox world

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -1,9 +1,18 @@
 using System;
+using System.Globalization;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Sandbox World!");
+        // capitalize
+        string randomWords = "hello sandbox world!";
+        TextInfo textInfo = CultureInfo.CurrentCulture.TextInfo;
+        string capitalize = textInfo.ToTitleCase(randomWords);
+        Console.WriteLine(capitalize);
     }
 }
+
+// python: .upper()  C#: .ToUpper()
+// python: .capitalize  C#: using System.Globalization; 
+// new sandbox: cd sandbox, dotnet new console -o Playground --use-program-main, cd test, dotnet build, cd Playground, dotnet run
