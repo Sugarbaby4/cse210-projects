c59a675bf2f4ca08c9b54f754085ab39946fde31
Sugarbaby4 <lew23028@byui.edu>
2025-09-24 11:39:05 -0600

    Numbers List & sort

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,74 @@
 using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Security;
+using System.Linq;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+
+        int userNumber = -1;
+        while (userNumber != 0)
+        {
+            Console.Write("Please Enter a Number (0 to quit): ");
+
+            string userResponse = Console.ReadLine();
+            userNumber = int.Parse(userResponse);
+
+            if (userNumber != 0)
+            {
+                numbers.Add(userNumber);
+            }
+        }
+
+        //Finding sum//
+        int sum = 0;
+
+        foreach (int number in numbers)
+        {
+            sum += number;
+        }
+
+        Console.WriteLine($"The sum is: {sum}");
+
+        //Finding Average//
+        float average = ((float)sum) / numbers.Count;
+        Console.WriteLine($"The average is: {average}");
+
+        //Find the Max//
+        int max = numbers[0];
+
+        foreach (int number in numbers)
+        {
+            if (number > max)
+            {
+                max = number;
+            }
+        }
+
+        Console.WriteLine($"The max is: {max}");
+
+        //Strech//
+        var positives = numbers.Where(n => n > 0);
+
+        if (positives.Any())
+        {
+            Console.WriteLine($"The min is: {positives.Min()}");
+        }
+        else
+        {
+            Console.WriteLine("No positive numbers found");
+        }
+
+        numbers.Sort();
+
+        Console.WriteLine("The sorted list is: ");
+        foreach (int number in numbers)
+        {
+            Console.WriteLine(number);
+        }
     }
 }
